28122025b3869c90cd3b6421b48112d035e7714e
石原誠 <makoto@ishiharotonoAir.hitronhub.home>
2016-12-21 11:07:45 -0800

    First task completed  * Create an auto loader(loader_makoto.php) in CSS JS Loader that loads jQuery v1.12.0 and a custom JS file(makoto.js) on all product listings.

---
+++ includes/templates/my_own_template/auto_loaders/loader_makoto.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* @package Pages
+* @copyright Makoto Ishihara
+* @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+*/
+
+//load files on pages below
+$loaders[] = array('conditions' => array('pages' => array('index_home', 'index', 'products_new', 'featured_products', 'products_all', 'advanced_search_result')),
+//load js files below (jquery file and my original file called 'makoto.js')
+										'jscript_files' => array(
+										  '//code.jquery.com/jquery-1.12.0.min.js' => 1,
+										//  'jquery/jquery-migrate-1.2.1.min.js' => 2,
+                      'makoto.js' => 5
+                    )/*,
+                    'css_files' => array(
+                      'fec_global.css' => 1,
+                      'auto_loaders/fec_overrides.css' => 2
+                    )*/
+								);
---
+++ includes/templates/my_own_template/jscript/makoto.js
@@ -0,0 +1,10 @@
+/*
+makoto.js
+
+Author: Makoto Ishihara
+Date : 2016/12/21
+Description: This file will load automatically on each product listing page by my_own_template/auto_loaders/loader_makoto.php
+*/
+
+//for debug to confirm auto loaded
+window.alert('load makoto.js');
