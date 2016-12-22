b1b4af753dc224271c7aee99760d0ad57ceb91b9
石原誠 <makoto@ishiharotonoAir.hitronhub.home>
2016-12-21 16:04:35 -0800

    confirmed posted values checking working well

--- ajax/ajax_makoto.php
+++ ajax/ajax_makoto.php
@@ -1,5 +1,32 @@
 <?php
+/*
+Author: Makoto Ishihara
+Date: 2016 12/21
+Description: Ajax handler for my_own_template/jscript/makoto.js. Return color code according to browser.
+*/
 
 $browserName = $_POST['browser_name'];
 $isMobile = $_POST['is_mobile'];
+
+//posted value check
+$browserNames = ['chrome', 'safari', 'opera', 'ie', 'firefox'];
+$isMobiles = ['false', 'true'];
+$errorFlag = true;
+for($i=0; $i < count($browserNames); $i++){
+	if($browserName === $browserNames[$i]){
+		for($j=0; $j < count($isMobiles); $j++){
+			$errorFlag = true;
+			if($isMobile === $isMobiles[$j]){
+				$errorFlag = false;
+				break 2;
+			}
+		}
+	}
+}
+if($errorFlag){
+	http_response_code(400);
+	echo 'post value error';
+	exit();
+}
+
 echo "$browserName.$isMobile";
